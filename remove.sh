a2b9b9dba8852c87c776afd49ba049af1d90c0c9
System Administrator <roarijo89@gmail.com>
2020-05-09 16:12:04 -0400

    Init

--- remove.sh
+++ remove.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 find debfiles -type f -name '*.deb' -delete
 rm -r Packages.bz2
-=======
-find debs -type f -name '*.debs' -delete
-rm -r Packages.bz2
->>>>>>> 8520fe85fa3157f39e41c9691d509908afffee94
