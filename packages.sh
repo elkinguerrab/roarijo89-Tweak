67ab124321e5e72491f76d1ac96cacce48ddb996
System Administrator <roarijo89@gmail.com>
2020-05-09 19:33:20 -0400

    Init

--- packages.sh
+++ packages.sh
@@ -4,3 +4,7 @@ dpkg-deb -bZgzip projects/tools debfiles
 dpkg-deb -bZgzip projects/inapp debfiles
 
 dpkg-deb -bZgzip projects/icons debfiles
+
+dpkg-deb -bZgzip projects/best debfiles
+
+dpkg-deb -bZgzip projects/tiny debfiles
