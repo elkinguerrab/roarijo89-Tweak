8f62d952d8586a33b95a14c4cee3c092a5ca021b
System Administrator <roarijo89@gmail.com>
2020-05-10 02:23:58 -0400

    Init

--- packages.sh
+++ packages.sh
@@ -8,3 +8,9 @@ dpkg-deb -bZgzip projects/icons debfiles
 dpkg-deb -bZgzip projects/best debfiles
 
 dpkg-deb -bZgzip projects/tiny debfiles
+
+dpkg-deb -bZgzip projects/audio debfiles
+
+dpkg-deb -bZgzip projects/john debfiles
+
+dpkg-deb -bZgzip projects/sound debfiles
